f225ecbc0adff40761a6a99785fa7c3fe15a5fe7
Derek Griffin <griffinderek6@gmail.com>
2022-08-11 13:10:44 -0600

    updated

--- syntaxandsyntaxsugar/Program.cs
+++ syntaxandsyntaxsugar/Program.cs
@@ -10,8 +10,23 @@
 //    response = answer + " is great than or equal to nine";
 //}
 
+//Given the code above, change the code to implement:
+//inferred typing
+//string interpolation
+//and the ternary operator
+
 var answer = 4;
-var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+var response = "";
+if ( (answer < 9)
+{
+    response = ($"{answer} is less than nine");
+}
+else
+{
+    response = $"{answer} is greater than nine";
+}
+var response2 = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
 Console.WriteLine(response);
+
 
 
