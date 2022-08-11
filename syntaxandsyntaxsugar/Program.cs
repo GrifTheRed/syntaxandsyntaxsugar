ce8f0775791957566c235bc919f4ec41d4fc3fa7
Derek Griffin <griffinderek6@gmail.com>
2022-08-11 08:37:07 -0600

    finished syntax

---
+++ syntaxandsyntaxsugar/Program.cs
@@ -0,0 +1,17 @@
+﻿//int answer = 4;
+//string response;
+//if (answer < 9)
+
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is great than or equal to nine";
+//}
+
+var answer = 4;
+var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+Console.WriteLine(response);
+
+
